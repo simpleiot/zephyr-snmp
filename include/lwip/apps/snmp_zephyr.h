7601dd4d25f5c764fcac970fe280025a26dc7e7e
Hein Tibosch <hein@htibosch.net>
2025-03-31 23:23:55 +0800

    Added a function snmp_loop() to lwip/apps/snmp_zephyr.h

--- include/lwip/apps/snmp_zephyr.h
+++ include/lwip/apps/snmp_zephyr.h
@@ -9,6 +9,13 @@ extern "C" {
  */
 extern void snmp_init(void);
 
+/**
+ * handle incomming requests.
+ * A call-back will be generated when necessary.
+ */
+extern void snmp_loop(void);
+
+
 #ifdef __cplusplus
 } /* extern "C" */
 #endif
