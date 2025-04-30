e4bf639153113fe866494ec41b149aa25f9f2de3
Hein Tibosch <hein@htibosch.net>
2025-05-01 06:19:41 +0800

    Update snmp_zephyr and CL.md

--- include/lwip/apps/snmp_zephyr.h
+++ include/lwip/apps/snmp_zephyr.h
@@ -9,41 +9,6 @@
 extern "C" {
 #endif
 
-/**
- * @brief Definition of a callback function.
- *
- * @param[in] oid The current OID that is requested.
- * @param[in] entry A struct with information about this call-back.
- *
- * @return The return value of the handler will be sent to the requester.
- */
-struct snmp_handler_entry;
-typedef int (*snmp_handler)(const char *oid, struct snmp_handler_entry * entry);
-
-/**
- * @brief Definition of a callback entry.
- *
- */
-struct snmp_handler_entry {
-  snmp_handler handler;            /**< The function that will be called. */
-  const char *prefix;              /**< The IOD for which it will be called. */
-  struct snmp_handler_entry *next; /**< The next handler in an array. */
-};
-
-/**
- * @brief Try to match an OID with the installed handlers.
- *        When a match is found, call the handler function.
- *        This function is private and should only be called from
- *        the SNMP library.
- */
-size_t snmp_private_call_handler(const char *prefix, void *value);
-
-/**
- * @brief Install a new SNMP callback function.
- *
- * @param[in] entry A description of the new callback.
- */
-void install_snmp_handler(struct snmp_handler_entry * entry);
 
 /**
  * @brief Starts SNMP Agent. It assumes that the network is up and
@@ -81,7 +46,10 @@ size_t zephyr_log( const char * format, ... )
 #endif
 ;
 
+/* A user provided function that will wake-up or interrupt any blocking call
+ * and call snmp_recv_packet() to receive an SNMP packet. */
 void snmp_recv_complete(int packet_id);
+
 #ifdef __cplusplus
 } /* extern "C" */
 #endif
